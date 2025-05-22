aaa87b56e94a8f974fa8ae1dd930fd268b97896e
Arusudo <trilloarnau@gmail.com>
2025-05-22 13:18:31 +0200

    Delete enlace.php

--- enlace.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $localhost= "192.168.12.183";
-    $usuario="root";
-    $clave="PHP@Mypass1";
-    $database="fp_dual";
-    $enlace = new mysqli ($localhost, $usuario, $clave, $database);
-    
-    if ()
-?>
