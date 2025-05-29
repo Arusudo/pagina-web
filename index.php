5768a0c77a1fe04da647fcdc0112f879bc83d8d5
Arusudo <trilloarnau@gmail.com>
2025-05-29 14:37:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
             <h1 id="h1ini">CPIFP Bajo Aragón</h1>
         </header>
         <main id="mainses" class="mains">
-            <div> 
-                <h1 id="h1form">Inicio de sesión</h1>
-            </div>
             <?php
             error_reporting(E_ALL);
             ini_set("display_errors", 1);
@@ -20,7 +17,11 @@
             include("conexion_bd.php");
             ?>
             <section>
+            <div id="divini">
             <form action="controlador.php" method="post" id="iniform"> 
+                 
+                    <h1 id="h1form">Inicio de sesión</h1>
+
                     <label for="uname">Usuario</label><br>
                     <input required type="text" placeholder="Introduce tu usuario" name="uname" id="uname" ><br>
 
@@ -29,6 +30,7 @@
 
                     <button type="submit" name="button">Acceder</button>
                 </form>
+            </div>
             </section>
         </main>
         <footer>
